217d4a8ae63e1ffb37f920eb5c33b526a5855c05
Chahatpreet Singh <c@chahat.dev>
2026-05-22 19:59:59 -0500

    Switch to flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Chahat's NixOS System Flake";
+
+  inputs = {
+    # Pins the system to the 25.11 stable branch
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
