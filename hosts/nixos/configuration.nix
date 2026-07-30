eaa07f87027818b77743bf0f91214699c76197e9
Chahatpreet Singh <c@chahat.dev>
2026-07-30 17:18:03 -0500

    Remove unused key binding from nixos machine

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -666,9 +666,6 @@
           # [Hyper] Right Alt → Right Alt (tap) / Hyper Layer (hold)
           # Keeps your right thumb exactly where it expects to be
           rightalt = "overload(hyper, rightalt)";
-
-          # [Right Shift] Right Shift -> Tab (tap) / Right Shift (hold)
-          rightshift = "overload(rightshift, tab)";
         };
 
         # The Hyper layer triggers your custom navigation and media controls
